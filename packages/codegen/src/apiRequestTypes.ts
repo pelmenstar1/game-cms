7497e9e4d62c0ae6b2bbba1a2942b58410c5ab60
Oleh Khmaruk <pelmenstar1@gmail.com>
2025-12-09 13:10:08 +0200

    feat: simplify glob

--- packages/codegen/src/apiRequestTypes.ts
+++ packages/codegen/src/apiRequestTypes.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 
 import { importFile } from '@game-cms/shared';
 import type { HttpMethod, UnknownApiRoute } from '@game-cms/types';
+import { glob } from 'glob';
 
 const routesDir = path.resolve('./src/routes');
 const outFileName = 'types.gen.ts';
@@ -15,32 +16,9 @@ type RouteInfo = {
 };
 
 async function scanRoutes(): Promise<string[]> {
-  async function worker(baseDir: string): Promise<string[]> {
-    const entries = await fsp.readdir(baseDir, { withFileTypes: true });
+  const result = await glob('./src/routes/**/*.ts');
 
-    const result = await Promise.all(
-      entries.map(async (entry) => {
-        const { name } = entry;
-        const entryPath = path.join(baseDir, name);
-
-        if (entry.isDirectory()) {
-          return worker(entryPath);
-        } else if (
-          entry.isFile() &&
-          name.endsWith('.ts') &&
-          name !== outFileName
-        ) {
-          return [entryPath];
-        }
-
-        return [];
-      })
-    );
-
-    return result.flat();
-  }
-
-  return worker(routesDir);
+  return result.filter((filePath) => !filePath.endsWith(outFileName));
 }
 
 async function transformRoute(filePath: string): Promise<RouteInfo> {
@@ -52,15 +30,15 @@ async function transformRoute(filePath: string): Promise<RouteInfo> {
 
 function createMetaFileContent(routes: RouteInfo[]) {
   const routesArray = routes.map(({ filePath, method, url }) => {
-    const relative = path.relative(routesDir, filePath);
+    const relative = path.relative(routesDir, filePath).replaceAll('\\', '/');
     const jsFile = relative.replace('.ts', '.js');
 
-    return `'${method} ${url}': typeof import('./${jsFile}')`;
+    return `    '${method} ${url}': typeof import('./${jsFile}');`;
   });
 
   let result = `declare module '@game-cms/types' {\n`;
   result += `  interface ApiRouteMap {\n`;
-  result += routesArray.join('    \n');
+  result += routesArray.join('\n');
   result += `\n  }\n`;
   result += '}\n';
 
@@ -73,6 +51,7 @@ async function main() {
   }
 
   const routes = await scanRoutes();
+
   const routeInfos = await Promise.all(
     routes.map((filePath) => transformRoute(filePath))
   );
--- packages/ui/scripts/copyStyles.ts
+++ packages/ui/scripts/copyStyles.ts
@@ -2,31 +2,12 @@ import fsp from 'node:fs/promises';
 import path from 'node:path';
 
 import { isEntityExistsError } from '@game-cms/shared/errors';
+import { glob } from 'glob';
 
 const srcDir = path.join(import.meta.dirname, '../src');
 
 async function findFiles(): Promise<string[]> {
-  async function worker(dirName: string): Promise<string[]> {
-    const entries = await fsp.readdir(dirName, { withFileTypes: true });
-
-    const result = await Promise.all(
-      entries.map(async (entry) => {
-        const entryPath = path.join(dirName, entry.name);
-
-        if (entry.isDirectory()) {
-          return worker(entryPath);
-        } else if (entry.isFile() && entry.name.endsWith('.scss')) {
-          return entryPath;
-        } else {
-          return [];
-        }
-      })
-    );
-
-    return result.flat();
-  }
-
-  return worker(srcDir);
+  return glob(path.join(srcDir, '**', '*.scss'));
 }
 
 async function copyStyles(files: string[]) {
