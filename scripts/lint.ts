33f558d8d1a5b745a7b3e830199716bcc7e7e3e6
Oleh Khmaruk <pelmenstar1@gmail.com>
2026-03-28 18:46:39 +0200

    chore: lint dx

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -1,8 +1,13 @@
 import { pnpm } from './process';
 
-const STYLELINT_PATTERN = '**/*.{css,scss}';
-const PRETTIER_PATTERN = '**.{ts,tsx,js,mjs,mts,json,css,scss,md,yml,html}';
-const ESLINT_COMMAND = 'eslint --cache --cache-location .eslintcache';
+const STYLELINT_COMMAND =
+  'stylelint --cache --cache-location node_modules/.cache/.stylelintcache "**/*.{css,scss}"';
+
+const PRETTIER_COMMAND =
+  'prettier --cache "**.{ts,tsx,js,mjs,mts,json,css,scss,md,yml,html}"';
+
+const ESLINT_COMMAND =
+  'eslint --cache --cache-location node_modules/.cache/.eslintcache';
 
 async function phase(name: string, command: string) {
   try {
@@ -12,7 +17,7 @@ async function phase(name: string, command: string) {
     console.log(`> ${name} finished`);
   } catch (error: unknown) {
     console.error(`> ${name}`);
-    console.error(error);
+    console.error(error instanceof Error ? error.message : error);
 
     throw error;
   }
@@ -24,26 +29,26 @@ async function main() {
   try {
     if (fix) {
       await phase('eslint', `${ESLINT_COMMAND} --fix`);
-      await phase('stylelint', `stylelint --cache ${STYLELINT_PATTERN} --fix`);
-      await phase('prettier', `prettier --cache --write ${PRETTIER_PATTERN}`);
+      await phase('stylelint', `${STYLELINT_COMMAND} --fix`);
+      await phase('prettier', `${PRETTIER_COMMAND} --write`);
     } else {
       const result = await Promise.allSettled([
         phase('eslint', ESLINT_COMMAND),
-        phase('stylelint', `stylelint --cache ${STYLELINT_PATTERN}`),
-        phase('prettier', `prettier --cache --check ${PRETTIER_PATTERN}`),
+        phase('stylelint', STYLELINT_COMMAND),
+        phase('prettier', `${PRETTIER_COMMAND} --check`),
         phase('build', 'tsc --build tsconfig.ref.json'),
         phase('knip', 'knip'),
       ]);
 
-      if (result.some((value) => value.status === 'rejected')) {
-        throw new Error('Lint failed');
+      if (result.every((value) => value.status === 'fulfilled')) {
+        return;
       }
     }
   } catch {
     // Messages about errors are already in the terminal
-
-    process.exit(1);
   }
+
+  process.exit(1);
 }
 
 void main();
