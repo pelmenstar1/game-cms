dabec6b1893d1494d59488fbd12339e4fd3899d4
Oleh Khmaruk <pelmenstar1@gmail.com>
2026-04-13 13:12:15 +0300

    chore: improve css names in dev mode

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -3,9 +3,15 @@ import { defineConfig } from 'vite';
 import { libraryWatcherPlugin } from '@game-cms/vite-plugins';
 import { ignitePlugin } from './plugins/ignite/plugin';
 
-export default defineConfig(() => ({
+export default defineConfig((env) => ({
   resolve: {
     tsconfigPaths: true,
+  },
+  css: {
+    modules: {
+      generateScopedName:
+        env.command === 'serve' ? '[name]_[local]_[hash:base64:5]' : undefined,
+    },
   },
   plugins: [
     reactRouter(),
