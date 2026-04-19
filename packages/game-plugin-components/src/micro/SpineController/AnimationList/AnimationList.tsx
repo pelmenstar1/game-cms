b66dac4a14c72e861696a6905f2331d024c52333
Oleh Khmaruk <pelmenstar1@gmail.com>
2026-04-19 13:20:37 +0300

    feat: search animation list

--- packages/game-plugin-components/src/micro/SpineController/AnimationList/AnimationList.tsx
+++ packages/game-plugin-components/src/micro/SpineController/AnimationList/AnimationList.tsx
@@ -1,6 +1,14 @@
-import { classNames, UnstyledOption } from '@game-cms/ui';
+import {
+  classNames,
+  SearchIcon,
+  TextInput,
+  UnstyledOption,
+} from '@game-cms/ui';
+import { useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import styles from './AnimationList.module.scss';
+import { filterAnimations } from './utils.js';
 
 export interface AnimationListProps {
   className?: string;
@@ -16,31 +24,53 @@ export function AnimationList({
   selectedAnimation,
   onAnimationSelected,
 }: AnimationListProps) {
+  const [query, setQuery] = useState('');
+
+  const filteredAnimations = useMemo(
+    () => filterAnimations(animations, query),
+    [animations, query]
+  );
+
+  const { t } = useTranslation('game', {
+    keyPrefix: 'micro.SpineController.AnimationList',
+  });
+
   return (
     <div className={classNames(styles['root'], className)}>
-      {animations.map((name) => {
-        const checked = selectedAnimation === name;
+      <TextInput
+        className={styles['search']}
+        value={query}
+        placeholder={t('search')}
+        variant="underline"
+        endContent={<SearchIcon />}
+        onTextChanged={setQuery}
+      />
 
-        const onChange = () => {
-          onAnimationSelected?.(name);
-        };
+      <div className={styles['list']}>
+        {filteredAnimations.map((name) => {
+          const checked = selectedAnimation === name;
 
-        return (
-          <UnstyledOption
-            className={classNames(
-              styles['root-item'],
-              checked && styles['root-item-checked']
-            )}
-            key={name}
-            type="radio"
-            weight="bold"
-            checked={checked}
-            onChange={onChange}
-          >
-            {name}
-          </UnstyledOption>
-        );
-      })}
+          const onChange = () => {
+            onAnimationSelected?.(name);
+          };
+
+          return (
+            <UnstyledOption
+              className={classNames(
+                styles['list-item'],
+                checked && styles['list-item-checked']
+              )}
+              key={name}
+              type="radio"
+              weight="bold"
+              checked={checked}
+              onChange={onChange}
+            >
+              {name}
+            </UnstyledOption>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- packages/ui/src/icons/google.tsx
+++ packages/ui/src/icons/google.tsx
@@ -1,12 +1,20 @@
 import type { FC } from 'react';
 
+import { classNames } from '../utils/classNames';
+import styles from './google.module.scss';
 import type { SvgProps } from './types';
 
 /*@__NO_SIDE_EFFECTS__*/
 export function googleIcon(d: string) {
-  const component: FC<SvgProps> = (props) => {
+  const component: FC<SvgProps> = ({ className, ...rest }) => {
     return (
-      <svg width="24px" height="24px" viewBox="0 -960 960 960" {...props}>
+      <svg
+        width="24px"
+        height="24px"
+        viewBox="0 -960 960 960"
+        className={classNames(styles['google-icon'], className)}
+        {...rest}
+      >
         <path d={d} />
       </svg>
     );
