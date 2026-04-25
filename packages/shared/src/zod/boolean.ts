7e44f4f638745451537a2cbe74593c45e42829fe
Oleh Khmaruk <pelmenstar1@gmail.com>
2026-04-25 22:34:01 +0300

    fix: stringBoolean

--- packages/shared/src/zod/boolean.ts
+++ packages/shared/src/zod/boolean.ts
@@ -1,14 +1,21 @@
 import z from 'zod';
 
 export const stringBoolean = z.string().transform((value, ctx) => {
-  if (value === 'true') {
-    return true;
-  } else if (value === 'false') {
-    return false;
-  } else {
-    ctx.addIssue({
-      code: 'invalid_value',
-      values: ['false', 'true'],
-    });
+  switch (value) {
+    case 'true': {
+      return true;
+    }
+    case 'false': {
+      return false;
+    }
+    default: {
+      ctx.addIssue({
+        code: 'invalid_value',
+        values: ['false', 'true'],
+        input: value,
+      });
+
+      return z.NEVER;
+    }
   }
 });
